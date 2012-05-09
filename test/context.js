fa58b58282a97b94d20bbcf34bc1b838ce8a8886
colacioiu <colacioiu@4e596854-7c54-45ca-8723-55143d051b2a>
2012-05-09 15:37:24 +0000

    added activate deactivate tests
    git-svn-id: https://adapt-lab.dico.unimi.it/svn/cop/trunk@139 4e596854-7c54-45ca-8723-55143d051b2a

--- test/context.js
+++ test/context.js
@@ -3,10 +3,20 @@ $(document).ready(function() {
   module("Cop.Context");
 
   test("Context: creation", function() {
-    var context = new Cop.Context({name: 'foo'});
+    var initializeFunc = function() {};
+    var destroyFunc = function() {};
+
+    var context = new Cop.Context({
+      name: 'foo',
+      initialize: initializeFunc,
+      destroy: destroyFunc
+    });
 
     equal(context.name, 'foo', "context name should be the same");
+    equal(context.active, false, "context should not be active");
     equal(context.adaptations.length, 0, "context should have no adaptations");
+    equal(context.initialize, initializeFunc, "context should have set initialize function correctly");
+    equal(context.destroy, destroyFunc, "context should have set destroy function correctly");
     
     try {
       new Cop.Context();
@@ -53,9 +63,49 @@ $(document).ready(function() {
 
     var adapted = context.getAdaptation(object);
 
-    equal(adapted.object, object, "should have been the same object");
-    equal(adapted.trait, trait, "should have been the same trait");
+    equal(adapted.object, object, "should be the same object");
+    equal(adapted.trait, trait, "should be the same trait");
 		equal(context.adaptations.length, 1, "should have 1 adapted object");
+  });
+
+  test("Context: activate deactivate", function() {
+    var context = new Cop.Context({name: 'test'});
+    var flag = undefined;
+
+    context.on("activate", function() {
+      flag = true;
+    });
+    context.on("deactivate", function() {
+      flag = false;
+    });
+
+    equal(context.active, false, "context should not be active on creation");
+    equal(flag, undefined, "flag should be undefined");
+
+    context.activate();
+
+    equal(context.active, true, "context should be active now");
+    equal(flag, true, "flag should be true");
+
+    context.activate();
+
+    equal(context.active, true, "calling activate again on context should not change state");
+    equal(flag, true, "also, flag should remain true");
+
+    context.deactivate();
+    
+    equal(context.active, false, "context should be deactivated now");
+    equal(flag, false, "flag should be false");
+
+    context.deactivate();
+    
+    equal(context.active, false, "calling deactivate again on context should not change state");
+    equal(flag, false, "flag should be false");
+
+    context.activate();
+
+    equal(context.active, true, "calling activate should make context active");
+    equal(flag, true, "flag should be true");
   });
 
 });
